7df9300d464c0d2ec6e03c3e215fe283210f7663
Devin Droddy <devin.droddy@gmail.com>
2024-11-12 10:54:21 -0500

    Just go back to putting it on pkgs...

--- flake.nix
+++ flake.nix
@@ -7,11 +7,7 @@
   outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;} {
       flake.overlays.default = final: prev: {
-        pythonPackagesOverlays =
-          prev.pythonPackagesOverlays
-          ++ [
-            (python-final: python-prev: {textual-autocomplete = final.callPackage ./package.nix {};})
-          ];
+        textual-autocomplete = final.callPackage ./package.nix {};
       };
       systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
       perSystem = {pkgs, ...}: {
