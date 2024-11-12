898098faf462f3a3f42b9acf830e9365f7116a5e
Devin Droddy <devin.droddy@gmail.com>
2024-11-12 13:25:40 -0500

    use `prev.callPackage` instead of `final`

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
   outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;} {
       flake.overlays.default = final: prev: {
-        textual-autocomplete = final.callPackage ./package.nix {};
+        textual-autocomplete = prev.callPackage ./package.nix {};
       };
       systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
       perSystem = {pkgs, ...}: {
