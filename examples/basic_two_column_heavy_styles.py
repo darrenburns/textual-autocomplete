81bf30ac9f9d4ae6e34838a6e4f22a9e07f2ded9
Darren Burns <darrenb900@gmail.com>
2025-03-17 17:22:52 +0000

    Improve example

--- examples/basic_two_column_heavy_styles.py
+++ examples/basic_two_column_heavy_styles.py
@@ -2,13 +2,14 @@
 
 from textual.app import App, ComposeResult
 from textual.content import Content
+from textual.style import Style
 from textual.widgets import Input, Label
 
-from textual_autocomplete._autocomplete import AutoComplete, DropdownItem
+from textual_autocomplete import AutoComplete, DropdownItem
 from examples._headers import headers
 
 # Define a mapping of sections to colors
-SECTION_COLORS = {
+SECTION_COLORS: dict[str, str] = {
     "Authentication": "$text-success",
     "Caching": "$text-primary",
     "Conditionals": "$text-warning",
@@ -24,11 +25,14 @@ SECTION_COLORS = {
 # Create dropdown items with two columns: rank and language name
 CANDIDATES = [
     DropdownItem(
-        Content.from_markup(
-            f"[italic {SECTION_COLORS.get(header.get('section', 'default'), SECTION_COLORS['default'])}]{header['name']}"
+        Content.styled(
+            header["name"],
+            style=SECTION_COLORS.get(
+                header.get("section", "default"), SECTION_COLORS["default"]
+            ),
         ),  # Main text to be completed with color based on section
         left_column=Content.from_markup(
-            f"[$text-primary on $primary-muted]{i:3} "
+            "[$text-primary on $primary-muted] $number", number=f"{i:<3}"
         ),  # Left column showing rank, styled with Textual markup!
     )
     for i, header in enumerate(headers)
@@ -37,7 +41,7 @@ CANDIDATES = [
 
 class TwoColumnAutoCompleteExample(App[None]):
     def compose(self) -> ComposeResult:
-        yield Label("Start typing a programming language:")
+        yield Label("Start typing an HTTP header name:")
         text_input = Input(placeholder="Type here...")
         yield text_input
 
